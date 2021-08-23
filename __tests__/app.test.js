b93964f5f6d955ae8317e00b3c37a7982eafa5a6
Simon Kaine <simonkaine@gmail.com>
2021-08-23 16:19:31 -0700

    passing GET test.

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -28,31 +28,49 @@ describe('app routes', () => {
       return client.end(done);
     });
 
-    test('returns animals', async() => {
+    test('GET/ returns todos', async() => {
 
       const expectation = [
         {
           'id': 1,
-          'name': 'bessie',
-          'cool_factor': 3,
-          'owner_id': 1
+          'todo': 'wash the dishes',
+          'completed': false,
+          'user_id': 1
         },
         {
           'id': 2,
-          'name': 'jumpy',
-          'cool_factor': 4,
-          'owner_id': 1
+          'todo': 'do the laundry',
+          'completed': false,
+          'user_id': 1
         },
         {
           'id': 3,
-          'name': 'spot',
-          'cool_factor': 10,
-          'owner_id': 1
+          'todo': 'clean the car',
+          'completed': false,
+          'user_id': 1
+        },
+        {
+          'id': 4,
+          'todo': 'water the garden',
+          'completed': false,
+          'user_id': 1
+        },
+        {
+          'id': 5,
+          'todo': 'mow the lawn',
+          'completed': false,
+          'user_id': 1
+        },
+        {
+          'id': 6,
+          'todo': 'clean the bathroom',
+          'completed': false,
+          'user_id': 1
         }
       ];
 
       const data = await fakeRequest(app)
-        .get('/animals')
+        .get('/todos')
         .expect('Content-Type', /json/)
         .expect(200);
 
